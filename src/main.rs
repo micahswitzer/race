a8fdccaab6ed56c908ac11acf082ca91b06751f6
Micah Switzer <micah.switzer.dev@outlook.com>
2021-05-23 12:45:56 -0400

    Support tracing each system call

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::convert::TryInto;
 use std::process::Command;
 use nix::sys::{ptrace, wait::{waitpid, WaitStatus::*}, signal::Signal::*};
+use nix::Error::Sys;
 use nix::unistd::Pid;
+use nix::errno::Errno::*;
 use libc::user_regs_struct;
 
 const PROC: &'static str = "./test-app";
+
+#[derive(Eq, PartialEq)]
+enum State {
+    Attach,
+    SyscallBefore,
+    SyscallAfter,
+}
 
 #[cfg(target_arch = "x86_64")]
 fn print_regs(regs: &user_regs_struct) {
@@ -32,22 +41,45 @@ pub fn main() {
     ptrace::attach(pid)
         .expect("Failed to attach to target process");
     println!("Attached to target process");
+    let mut state = State::Attach;
     loop {
         let status = waitpid(pid, None)
             .expect("Failed to waitpid");
         println!("{} is {:?}", pid, status);
+        if let Exited(_, _) = status { break; }
+        if state == State::SyscallBefore {
+            println!("Before syscall:");
+        } else if state == State::SyscallAfter {
+            println!("After syscall");
+        }
         print_regs(&ptrace::getregs(pid).expect("Failed to get the process' registers"));
         match status {
-            Stopped(_, SIGSTOP) => break,
-            Stopped(lpid, sig) => ptrace::cont(lpid,
-                                    match sig { SIGTRAP => None, _ => Some(sig) })
-                .expect("Failed to ptrace::cont"),
+            Stopped(lpid, SIGSTOP) => {
+                ptrace::syscall(lpid, None).expect("Failed to ptrace::syscall");
+                state = State::SyscallBefore;
+            },
+            Stopped(lpid, sig) => {
+                ptrace::syscall(lpid, match sig { SIGTRAP => None, _ => Some(sig) })
+                    .expect("Failed to ptrace::cont");
+                if sig == SIGTRAP {
+                    state = match state {
+                        State::SyscallBefore => State::SyscallAfter,
+                        State::SyscallAfter => State::SyscallBefore,
+                        _ => State::SyscallBefore
+                    };
+                }
+            },
             _ => panic!("Bad status after waitpid: {:?}", status)
         }
     }
-    ptrace::detach(pid, None)
-        .expect("Failed to detach from target process");
-    println!("Detached from target process");
+    match ptrace::detach(pid, None) {
+        Err(Sys(ESRCH)) => { 
+            println!("Target process has already exited");
+            return;
+        },
+        Err(err) => println!("Error while detaching: {}", err),
+        Ok(()) => println!("Detached from target process")
+    }
     let status = child.wait()
         .expect("Failed to wait for target process to exit");
     println!("Target exited with code {}", status.code().unwrap());
